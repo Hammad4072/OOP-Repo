0b4238467b9ff8badc1f78788032933498591446
Hammad Mudassar <it.hammadmudassar5@gmail.com>
2025-07-11 16:11:25 +0500

    add oop.py file

---
+++ oop.py
@@ -0,0 +1,8 @@
+class pet:
+    def __init__(self, name , age):
+        self.name = name
+        self.age = age
+
+obj = pet("labre", "2 years")
+
+print("the dog name is:",obj.name , "and its age is", obj.age)
