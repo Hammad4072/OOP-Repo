c901970ec674df7e11824a1ec38b010a1f990e0c
Hammad Mudassar <it.hammadmudassar5@gmail.com>
2025-07-11 16:39:56 +0500

    encapsulation added

--- oop.py
+++ oop.py
@@ -1,8 +1,13 @@
 class pet:
     def __init__(self, name , age):
-        self.name = name
-        self.age = age
-
+        #encapsulation:
+        self.__name = name
+        self.__age = age
+    def get_name(self):
+        return f"{self.__name}"
+    def get_age(self):
+        return f"{self.__age}"
+    
 obj = pet("labre", "2 years")
-
-print("the dog name is:",obj.name , "and its age is", obj.age)
+print("My pet name is :", obj.get_name())
+print("My pet age is :", obj.get_age())
