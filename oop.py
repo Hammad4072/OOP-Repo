5721a26b677de3b33524dd56c3111d79894f12b2
Hammad Mudassar <it.hammadmudassar5@gmail.com>
2025-07-11 17:37:36 +0500

    added abstraction and minor inheritence

--- oop.py
+++ oop.py
@@ -1,13 +1,37 @@
-class pet:
+#Abstraction:
+from abc import ABC , abstractmethod
+class pet(ABC):
     def __init__(self, name , age):
-        #encapsulation:
+     
         self.__name = name
         self.__age = age
     def get_name(self):
-        return f"{self.__name}"
+        return self.__name
     def get_age(self):
-        return f"{self.__age}"
-    
-obj = pet("labre", "2 years")
-print("My pet name is :", obj.get_name())
-print("My pet age is :", obj.get_age())
+        return self.__age
+    def set_name(self, name):
+        self.__name = name
+    def set_age(self, age):
+        self.__age = age
+    @abstractmethod
+    def show_info(self):
+        pass
+
+#inheritence
+
+class Dog(pet):
+    def show_info(self):
+        print("the dog name is :", self.get_name())
+
+
+obj = Dog("labre", 3)
+obj.show_info()
+
+# obj = pet("labre" , "2 years")   
+# print(obj.get_name())
+
+
+
+
+# print("My pet name is :", obj.get_name())
+# print("My pet age is :", obj.get_age())
